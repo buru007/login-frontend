05251d2faac271fb35730faedecd6f1fdee0bf54
unknown <buru114@gmail.com>
2025-01-29 17:10:04 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignupPage from "./Pages/signup";
 import LoginPage from "./Pages/login";
+import HomePage from "./Pages/homepage";  // Moved HomePage to a separate file
+import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Default Route to Show a Home Page */}
-        <Route path="/" element={<h1>Welcome to the Login page</h1>} />
+        {/* Default Route to Show a Home Page with Buttons */}
+        <Route path="/" element={<HomePage />} />
         
         {/* Authentication Routes */}
         <Route path="/signup" element={<SignupPage />} />
---
+++ src/Pages/homepage.js
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import "../App.css"; 
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Login Page</h1>
+      <div className="button-container">
+        <button className="home-button" onClick={() => navigate("/signup")}>Signup</button>
+        <button className="home-button" onClick={() => navigate("/login")}>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
