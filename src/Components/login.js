47236492ee1af8bbd28b22055ebb1eedc1437458
unknown <buru114@gmail.com>
2025-01-30 08:59:43 +0530

    updated login page

--- src/Components/login.js
+++ src/Components/login.js
@@ -19,8 +19,28 @@ const Login = ({ onLogin }) => {
     <div>
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
-        <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+
         <button type="submit">Login</button>
       </form>
     </div>
